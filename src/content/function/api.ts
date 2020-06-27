4472e99f822001a21929e2ac2daa09aeed097cc2
llzsrt <pencil0325@gmail.com>
2020-06-27 08:08:13 +0800

    headers

--- src/content/function/api.ts
+++ src/content/function/api.ts
@@ -1,12 +1,7 @@
 function generateHeaders(token: string) {
     return {
         "accept": "application/json, text/plain, */*",
-        "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "token": token,
-        "x-requested-with": "XMLHttpRequest"
+        "token": token
     }
 }
 
